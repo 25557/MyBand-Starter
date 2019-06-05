92f9fca52347eaa9d73b21ab2aae62dfadaa2a12
25557 <43168595+25557@users.noreply.github.com>
2019-06-05 17:08:20 +0200

    Add files via upload

---
+++ private/views/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="T X H Nieuws">
+    <meta name="author" content="T X H">
+    <meta name="copyright" content="Tin Xian Hu">
+    <link rel="stylesheet" href="css/menu-style.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Koi Design</title>
+</head>
+
+<body class="site">
+<header>
+    <div class="menu-toggle" id="hamburger">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <div class="overlay"></div>
+    <div class="container">
+        <nav>
+            <h1 class="brand"><a href="#">T X H</a></h1>
+            <ul>
+                <li><a href="#">Voorpagina</a></li>
+                <li><a href="#">Over Ons</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<script src="js/menu.js"></script>
---
+++ private/views/home.php
@@ -0,0 +1,40 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body id="site">
+    <div class="content">
+      <div class="content-left">
+        <img src="img/minecraft.png" alt="">
+        <h3>Minecraft Earth: je nieuwe<br>Pokémon Go-verslaving is hier</h3>
+        <p>Microsoft heeft een nieuwe Minecraft-game aangekondigd<br>voor Android en iOS. De game werkt net als Pokémon Go<br>met augmented reality (AR) waarbij het spel zich afspeelt<br>in de wereld om de speler heen.</p>
+      </div>
+      <div class="content-right">
+        <img src="img/ing.png" alt="">
+        <h3>Apple Pay komt naar<br>Nederland via ING-bank</h3>
+        <p>De Nederlandse bank ING kondigt aan 'binnenkort' de<br>betaaldienst Apple Pay in Nederland te gaan aanbieden.<br>Een exacte datum of periode voor de introductie van de<Br>betaaldienst heeft de bank nog niet gegeven.</p>
+      </div>
+    </div>
+    <div class="windows">
+      <div class="content-left">
+        <img src="img/avengers.png" alt="">
+        <h3>De 9 box office records die<br>Avengers: Endgame verpulvert</h3>
+        <p>Endgame maakt korte metten met allerlei<br>records.</p>
+      </div>
+      <div class="content-center">
+        <img src="img/oneplus.png" alt="">
+        <h3>Kan OnePlus de strijd aan<br>met Samsung en Huawei?</h3>
+        <p>Kan OnePlus zich meten met de<br>toptoestellen van Samsung en Huawei?</p>
+      </div>
+      <div class="content-right">
+        <img src="img/minecraft.png" alt="">
+        <h3>Wanneer kun je in Nederland <br>5G gaan gebruiken?</h3>
+        <p>Nieuwe technologie in telecommasten<br>moet razendsnel verbinding gaan maken met <br>smartphones.</p>
+      </div>
+    </div>
+    <h2>1,2,3</h2>
+  </body>
+</html>
