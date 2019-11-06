39c7384397f61da7ee75b4195e2fce9721646e6d
Tin Xian <43168595+tinxian@users.noreply.github.com>
2019-11-06 18:51:42 +0100

    Update config.php

--- private/includes/config.php
+++ private/includes/config.php
@@ -2,9 +2,9 @@
 
 $config = [
     'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'c3680myband',
-    'DB_USER' => 'c3680myband',
-    'DB_PASSWORD' => 'root123',
+    'DB_NAME' => '',
+    'DB_USER' => '',
+    'DB_PASSWORD' => '',
     'BASE_URL' => '/myband/public',
     'ROOT' => dirname(dirname(__DIR__)),
     'PRIVATE' => dirname(__DIR__),
